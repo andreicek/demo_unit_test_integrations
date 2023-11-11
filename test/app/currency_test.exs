19cb0299c0c71f92a80c162d6f3acc13f5fa8daf
Andrei Zvonimir Crnkovic <andreicek@0x7f.dev>
2023-11-11 08:50:33 +0100

    Allow returning a custom response

--- test/app/currency_test.exs
+++ test/app/currency_test.exs
@@ -1,15 +1,13 @@
 defmodule App.CurrencyTest do
   use ExUnit.Case, async: true
 
-  import App.CurrencyHelpers, only: [bypass_currency: 0]
+  import App.CurrencyHelpers, only: [bypass_response: 1]
 
   alias App.Currency
 
   describe "get_rates/0" do
     test "returns a response with currency rates" do
-      Bypass.expect(bypass_currency(), fn conn ->
-        Plug.Conn.resp(conn, 200, "")
-      end)
+      bypass_response("currency_response")
 
       assert %Req.Response{status: 200} = Currency.get_rates()
     end
--- test/support/currency_helpers.ex
+++ test/support/currency_helpers.ex
@@ -1,7 +1,19 @@
 Mox.defmock(App.Currency.TestBaseUrl, for: App.Currency.BaseUrl)
 
 defmodule App.CurrencyHelpers do
-  def bypass_currency() do
+  def bypass_response(fixture_name) do
+    fixture = File.read!("test/support/fixtures/request_recordings/#{fixture_name}.json")
+
+    Bypass.expect(bypass_currency(), fn conn ->
+      conn
+      |> Plug.Conn.put_resp_content_type("application/json")
+      |> Plug.Conn.resp(200, fixture)
+    end)
+  end
+
+  ###
+
+  defp bypass_currency() do
     bypass = Bypass.open()
     url = "http://localhost:#{bypass.port}"
     Mox.expect(App.Currency.TestBaseUrl, :get_base_url, fn -> url end)
